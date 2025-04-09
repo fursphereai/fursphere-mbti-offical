76357ee8e48ac87f4c9a86e0bb09092480d8eb9e
keyi li <keyili@keyis-MacBook-Pro.local>
2025-04-09 12:15:53 -0500

    jason's update

--- src/app/components/mbti/get_result.tsx
+++ src/app/components/mbti/get_result.tsx
@@ -5,11 +5,12 @@ import axios from 'axios';
 export const getResult = async (submissionId: number): Promise<any> => {
   try {
     const response = await axios.get(`/api/proxy?submissionId=${submissionId}`);
-    console.log(response);
+    console.log('response.data.status', response.data.status);
     
     if (response.data.status === 'processing') {
       // If still processing, wait 1 second and try again
       await new Promise(resolve => setTimeout(resolve, 1000));
+      console.log('waiting for 1 second');
       return getResult(submissionId);
     }
     

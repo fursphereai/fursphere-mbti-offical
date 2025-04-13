58ab66b36a195e0209c770dc3a4c5aadb5f83e89
keyi li <keyili@keyis-MacBook-Pro.local>
2025-04-13 13:58:27 -0500

    jason's update

--- src/app/components/mbti/loggin_signup/email.tsx
+++ src/app/components/mbti/loggin_signup/email.tsx
@@ -271,43 +271,6 @@ return (
           }}>
 
 
-     <div className="
-      
-      
-      md:hidden
-      h-[56px]
-  
-      border-b-[0.8px] border-[#C9C9C9]
-      flex items-center justify-between
-      px-[20px]
-      "> 
-        <button className="w-[24px] h-[24px]"
-        onClick={() => {
-        
-            setShowEmail(false);
-          
-        }}
-         >
-       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <g opacity="0.7">
-        <path d="M20 4L4 20M4 4L20 20" stroke="#27355D" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-        </g>
-       </svg> 
-       
-    </button>
-      <h1 className="text-[16px] font-[400]  text-[Inter] text-[#101828]">
-        Get Result
-      </h1>
-       <button className="w-[24px] h-[24px]"
-        onClick={handleShare}>
-       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-
-        <g opacity="0.7">
-        <path d="M8.7 10.7L15.3 7.3M8.7 13.3L15.3 16.7M3 12C3 12.7956 3.31607 13.5587 3.87868 14.1213C4.44129 14.6839 5.20435 15 6 15C6.79565 15 7.55871 14.6839 8.12132 14.1213C8.68393 13.5587 9 12.7956 9 12C9 11.2044 8.68393 10.4413 8.12132 9.87868C7.55871 9.31607 6.79565 9 6 9C5.20435 9 4.44129 9.31607 3.87868 9.87868C3.31607 10.4413 3 11.2044 3 12ZM15 6C15 6.79565 15.3161 7.55871 15.8787 8.12132C16.4413 8.68393 17.2044 9 18 9C18.7956 9 19.5587 8.68393 20.1213 8.12132C20.6839 7.55871 21 6.79565 21 6C21 5.20435 20.6839 4.44129 20.1213 3.87868C19.5587 3.31607 18.7956 3 18 3C17.2044 3 16.4413 3.31607 15.8787 3.87868C15.3161 4.44129 15 5.20435 15 6ZM15 18C15 18.7956 15.3161 19.5587 15.8787 20.1213C16.4413 20.6839 17.2044 21 18 21C18.7956 21 19.5587 20.6839 20.1213 20.1213C20.6839 19.5587 21 18.7956 21 18C21 17.2044 20.6839 16.4413 20.1213 15.8787C19.5587 15.3161 18.7956 15 18 15C17.2044 15 16.4413 15.3161 15.8787 15.8787C15.3161 16.4413 15 17.2044 15 18Z" stroke="#27355D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        </g>
-       </svg>
-      </button>
-    </div>
 
           {/* <div className="md:hidden">
             <MobileHeader
@@ -347,9 +310,48 @@ return (
              />
           </div>
 
-        <div className="flex flex-col items-center justify-center w-auto h-auto mt-[40px] md:mt-[80px]">
+        <div className="bg-blue-300 flex flex-col items-center justify-center w-auto h-auto mt-[0px] md:mt-[80px]">
+           {/* //header */}
+           <div className="
+      
+            
+            md:hidden
+            h-[56px]
+            w-full
+            border-b-[0.8px] border-[#C9C9C9]
+            flex items-center justify-between
+            px-[20px]
+            "> 
+            <button className="w-[24px] h-[24px]"
+            onClick={() => {
+            
+                setShowEmail(false);
+              
+            }}
+            >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <g opacity="0.7">
+              <path d="M20 4L4 20M4 4L20 20" stroke="#27355D" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+              </g>
+            </svg> 
+            
+          </button>
+            <h1 className="text-[16px] font-[400]  text-[Inter] text-[#101828]">
+              Get Result
+            </h1>
+            <button className="w-[24px] h-[24px]"
+              onClick={handleShare}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 
-          <div className="text-[32px] md:text-[48px] font-[Ubuntu] font-[500] text-[#505D90] leading-[38.4px] md:leading-[57.6px]">
+              <g opacity="0.7">
+              <path d="M8.7 10.7L15.3 7.3M8.7 13.3L15.3 16.7M3 12C3 12.7956 3.31607 13.5587 3.87868 14.1213C4.44129 14.6839 5.20435 15 6 15C6.79565 15 7.55871 14.6839 8.12132 14.1213C8.68393 13.5587 9 12.7956 9 12C9 11.2044 8.68393 10.4413 8.12132 9.87868C7.55871 9.31607 6.79565 9 6 9C5.20435 9 4.44129 9.31607 3.87868 9.87868C3.31607 10.4413 3 11.2044 3 12ZM15 6C15 6.79565 15.3161 7.55871 15.8787 8.12132C16.4413 8.68393 17.2044 9 18 9C18.7956 9 19.5587 8.68393 20.1213 8.12132C20.6839 7.55871 21 6.79565 21 6C21 5.20435 20.6839 4.44129 20.1213 3.87868C19.5587 3.31607 18.7956 3 18 3C17.2044 3 16.4413 3.31607 15.8787 3.87868C15.3161 4.44129 15 5.20435 15 6ZM15 18C15 18.7956 15.3161 19.5587 15.8787 20.1213C16.4413 20.6839 17.2044 21 18 21C18.7956 21 19.5587 20.6839 20.1213 20.1213C20.6839 19.5587 21 18.7956 21 18C21 17.2044 20.6839 16.4413 20.1213 15.8787C19.5587 15.3161 18.7956 15 18 15C17.2044 15 16.4413 15.3161 15.8787 15.8787C15.3161 16.4413 15 17.2044 15 18Z" stroke="#27355D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              </g>
+            </svg>
+            </button>
+                </div>
+
+           {/* //logo */}
+          <div className="text-[32px] md:text-[48px] font-[Ubuntu] font-[500] text-[#505D90] leading-[38.4px] md:leading-[57.6px] mt-[40px] md:mt-[0px]">
             <h2>🐾</h2>
           </div>
           <div className="text-[32px] md:text-[48px] font-[Ubuntu] font-[600] text-[#505D90] leading-[38.4px] md:leading-[57.6px] line-height-[38.4px] md:line-height-[57.6px] tracking-[-1.28px] md:tracking-[-1.92px]">
@@ -481,7 +483,7 @@ return (
           </div>
 
           {/* mobile version */}
-          <div className="md:hidden absolute bottom-[124px] max-w-[242px] text-center w-full text-[14px] font-[Inter] font-[400] text-[#717680] leading-[16px] line-height-[16.8px] flex flex-col items-center">
+          <div className=" md:hidden absolute bottom-[124px] max-w-[242px] text-center w-full text-[14px] font-[Inter] font-[400] text-[#717680] leading-[16px] line-height-[16.8px] flex flex-col items-center">
             *Each email address can access the<br />
             test result only once. 
             <br />

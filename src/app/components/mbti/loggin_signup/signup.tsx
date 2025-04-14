21cd3b5067cedafc1a400097714517b0dd52673c
keyi li <keyili@keyis-MacBook-Pro.local>
2025-04-14 15:15:38 -0500

    jason's update

--- src/app/components/mbti/loggin_signup/signup.tsx
+++ src/app/components/mbti/loggin_signup/signup.tsx
@@ -91,6 +91,20 @@ export default function Signup({ handleNext, setStep, setShowSignup, setShowLogi
             return emailRegex.test(email.trim());
         };
 
+
+        // const waitcheck = async (email: string) => {
+        //   const response = await checkingSignup(email);
+        //   if (JSON.stringify(response).includes('true')) {
+        //     setShowEmailAssociated(true);
+          
+        //     setSignupEmailAvailable(true);
+        //    } else {
+      
+        //     setSignupEmailAvailable(false);
+   
+        //    }
+        // };
+
         const checkEmailAvailability = async (email: string) => {
             try {
               console.log("airai email" + email);
@@ -99,20 +113,16 @@ export default function Signup({ handleNext, setStep, setShowSignup, setShowLogi
               console.log("airai" + JSON.stringify(response));
             if (JSON.stringify(response).includes('true')) {
                 setShowEmailAssociated(true);
-                setSignupChecking(true);
+           
                 setSignupEmailAvailable(true);
             } else {
-                setSignupChecking(true);
-
-            
                 setSignupEmailAvailable(false);
        
             }
             } catch (error) {
             console.error('Error checking email:', error);
             } finally {
-            setSignupChecking(false);
-            console.log("isChecking" + signupChecking);
+              
             }
         };
 
@@ -413,18 +423,7 @@ const validateAndSubmit = async () => {
             : 
                 'Send Code'}
             </button>
-{/* 
-            {signupChecking && (
-                    <div className="mt-2 text-gray-500 text-sm font-[Inter] flex items-center">
-                        <svg className="animate-spin h-4 w-4 mr-2" viewBox="0 0 24 24">
-                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fill="none" />
-                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
-                        </svg>
-                        Checking email availability...
-                    </div>
-            )} */}
 
-          </div>
 
 
 
@@ -442,7 +441,7 @@ const validateAndSubmit = async () => {
 
 
 
-          { showEmailAssociated && (
+          {/* { showEmailAssociated && (
             <div className="hidden md:flex flex-col mt-[8px] items-center justify-center max-w-[467px] w-full text-[#5777D0] text-[14px] font-[Inter] leading-[14px]">
                     <div className=" w-full flex flex-row justify-start items-center">
                     Oops, this email is already associated with an account. You can keep 
@@ -460,9 +459,8 @@ const validateAndSubmit = async () => {
                         </span>
                         here or try a different one.
                     </div>
-            </div>  ) 
-            
-        }
+            </div>  )  
+        } */}
 
           {showEmailAssociated && (
             <div className="flex md:hidden flex-col mt-[8px] items-center justify-center max-w-[320px] w-full text-[#5777D0] text-[14px] font-[Inter] leading-[14px]">
